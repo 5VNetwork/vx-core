ce479eb1346619e5cc742a11f599bf8815778819
x <5vnetwork@proton.me>
2026-02-12 14:09:27 +0800

    tester refactor

--- app/tester/tester.go
+++ app/tester/tester.go
@@ -25,7 +25,7 @@ type Tester struct {
 	SpeedTestFunc  func(ctx context.Context, h i.Outbound) (int64, error)
 	UsableTestFunc func(ctx context.Context, h i.Outbound) (bool, error)
 	PingTestFunc   func(ctx context.Context, h i.Outbound) (int, error)
-	ResultReporter
+	ResultReporter ResultReporter
 	// track ongoing tests
 	ongoingUsableTests sync.Map // key is handler, value is *testUsableResult
 }
@@ -42,7 +42,7 @@ func (t *Tester) TestPing(ctx context.Context, h i.Outbound) int {
 		log.Debug().Err(err).Str("handler", h.Tag()).Msg("ping test func error")
 		ping = -1
 	}
-	t.PingResult(h.Tag(), ping)
+	t.ResultReporter.PingResult(h.Tag(), ping)
 	return ping
 }
 
@@ -51,7 +51,7 @@ func (t *Tester) TestIPv6(ctx context.Context, h i.Outbound) bool {
 	if err != nil {
 		log.Debug().Err(err).Str("handler", h.Tag()).Msg("ipv6 test func error")
 	}
-	t.IPv6Result(h.Tag(), yes)
+	t.ResultReporter.IPv6Result(h.Tag(), yes)
 	log.Debug().Bool("yes", yes).Str("handler", h.Tag()).Msg("ipv6 test done")
 	return yes
 }
@@ -77,7 +77,7 @@ func (t *Tester) TestSpeed(ctx context.Context, h i.Outbound, rtry bool) int64 {
 		speed = -1
 	}
 
-	t.SpeedResult(h.Tag(), speed)
+	t.ResultReporter.SpeedResult(h.Tag(), speed)
 	return speed
 }
 
@@ -115,7 +115,7 @@ func (t *Tester) TestUsable(ctx context.Context, h i.Outbound, retry bool) bool 
 		time.Sleep(time.Millisecond * 1000)
 	}
 
-	t.UsableResult(h.Tag(), ok)
+	t.ResultReporter.UsableResult(h.Tag(), ok)
 
 	// Update result and signal completion
 	result.result = ok
